896671da9fef982f14383b751b8fc4a4e958a5cc
Dillon Brady <dillonebrady@gmail.com>
2016-10-19 16:35:57 -0400

    Initial commit

---
+++ c08/data/data-jsonp.js
@@ -0,0 +1,19 @@
+showEvents({
+  "events": [
+    {
+      "location": "Richmond, VA",
+      "date": "Nov 5 - 8",
+      "map": "img/map-ca.png"
+    },
+    {
+      "location": "Charlottesville, VA",
+      "date": "Nov 8 - 10",
+      "map": "img/map-tx.png"
+    },
+    {
+      "location": "Chapel Hill, NC",
+      "date": "Nov 10 - 13",
+      "map": "img/map-ny.png"
+    }
+  ]
+});
